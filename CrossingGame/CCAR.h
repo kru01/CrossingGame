dee2662a52d9440e1c5d7add7b324bf048be3c54
vmathu <anhthuvominh1@gmail.com>
2022-10-15 15:53:26 +0700

    change car's design and constants

--- CrossingGame/CCAR.h
+++ CrossingGame/CCAR.h
@@ -2,7 +2,7 @@
 #include "CVEHICLE.h"
 #include "CCONSTANT.h"
 
-const int CAR_WIDTH = 13, CAR_HEIGHT = 3;
+const int CAR_WIDTH = 13, CAR_HEIGHT = 4;
 const int CAR_COLOR = colors::BLACK;
 const POINT CAR_SPAWN_COORD = { fieldConstraints::BOUND_LEFT, fieldConstraints::BOUND_BOTTOM - CAR_HEIGHT };
 
